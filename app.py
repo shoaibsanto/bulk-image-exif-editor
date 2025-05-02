a47360419f6cda744be092536843091274931395
Shoaib Santo <44254821+shoaibsanto@users.noreply.github.com>
2025-05-03 00:56:12 +0600

    app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def add_exif_data(img, title, subject, tags, comments, gps_latitude, gps_longitu
     return output
 
 # --- Streamlit App ---
-st.title("Bulk Image EXIF Metadata Optimizer")
+st.title("Bulk Image Optimizer")
 
 uploaded_files = st.file_uploader(
     "Upload Images", 
